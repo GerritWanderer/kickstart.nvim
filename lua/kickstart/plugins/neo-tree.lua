65e3f236b2477de18d1bd98a8a88608245f46503
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2025-09-13 22:21:29 +0200

    use flash for navigation and enable follow current file in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,14 +14,30 @@ return {
     { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
+    window = {
+      mappings = {
+        ['s'] = false,
+      },
+    },
+    buffers = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
         hide_gitignored = false,
       },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
       window = {
         mappings = {
+          ['s'] = false,
           ['<leader>e'] = 'close_window',
           ['l'] = 'open',
           ['h'] = 'close_node',
